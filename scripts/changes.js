98db8d50a70ed9a2251c2e38b872d6f4a6ab55e7
Alessandro Y. Longo <luneth97@gmail.com>
2022-10-12 17:16:38 +0200

    Update changes.js

--- scripts/changes.js
+++ scripts/changes.js
@@ -347,11 +347,6 @@ $(document).ready(function(){
 
 /*SIDEBAR SCRIPT*/
 
-jQuery(document).ready(function ( $ ) {
-    $('[data-toggle=offcanvas]').click(function () {
-        $('.row-offcanvas').toggleClass('active');
-    });
-});
 
 
 
@@ -381,21 +376,6 @@ function closeNav() {
     $('.banner_issue').css('paddingLeft', '0');
 }
 
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-}
 /*END OF SIDEBAR SCRIPT*/
 
 /*METADATA SCRIPT: UNDERLINE*/
